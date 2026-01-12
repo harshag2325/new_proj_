2e66907449e40e45fe0ba455212a2688cfaf625d
Harsh Agrawal <ha0744106@gmail.com>
2026-01-13 01:16:22 +0530

    Refactor database connection and add CRUD operations
    Refactored database connection setup and added CRUD functions.

--- dao.py
+++ dao.py
@@ -1,7 +1,23 @@
 from db_config import connect_func
+db = connect_func('car_DB')
+collection = db.CarDetails
 def connect():
-    db = connect_func('car_DB')
-    collection = db.CarDetails
     data_list = collection.find({}, {'_id':False})
     return data_list
 
+def get_connection_insert(name, milege, model, id, color):
+    post_id = collection.insert_one({"Color": color, "Milege":milege, "Model": model, "Name": name, "Id":id})
+    
+    if post_id is not None:
+        return True
+    return False
+def get_connection_update(id, type, val):
+    post_id = collection.update_one({"id" : id}, {"$set":{type:val}})
+    if post_id is not None:
+        return True
+    return False
+def get_connection_delete(id):
+    post_id = collection.delete_one({"id": id})
+    if post_id is not None:
+        return True
+    return False
