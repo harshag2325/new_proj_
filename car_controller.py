13042a3c00f372b338f40fbf897f3fe410ff111a
Harsh Agrawal <ha0744106@gmail.com>
2026-01-13 01:15:48 +0530

    Enhance car management endpoints with error handling
    Added error handling and improved endpoints for car operations.

--- car_controller.py
+++ car_controller.py
@@ -10,21 +10,31 @@ def read_root():
 
 @app.get("/show_all")
 def read():
-    return ser.get_data()
+    if ser.get_data() is not None:
+        return ser.get_data()
+    return {"Data not found"}
 
 @app.get("/getCarById/{id}")
 def read_item(id: int):
-    pass
+    if ser.get_certain_data(id) is not None:
+        return ser.get_certain_data(id)
+    return {"Data not found"}
 
-@app.post("/addcar")
-def add_car():
-    pass
+@app.get("/addcar/{name}/{milege}/{model}/{id}/{color}")
+def add_car(name:str, milege:float, model:str, id:int, color:str):
+    if ser.create_car(name, milege, model, id, color):
+        return {"Car is created"}
+    return {"Car is not created"}
 
-@app.put("/modifyCar/{id}")
-def modify_car():
-    pass
+@app.get("/modifyCar/{id}/{type}/{val}")
+def modify_car(id:int, type: str, val: int | str | float):
+    if ser.modify_car(id, type, val):
+        return {"Car is Updated"}
+    return {"Car is not Updated"}
 
-@app.delete("/deleteCar")
-def delete_car():
-    pass
+@app.get("/deleteCar/{id}")
+def delete_car(id: int):
+    if ser.delete_car(id):
+        return {"Car is Deleted"}
+    return {"Car is not deleted"}
 
